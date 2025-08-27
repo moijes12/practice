8e9e9f6658b0364f9bbb8f6eedd3f125d6ec2709
Alexander Moses <moijes12@gmail.com>
2025-08-27 12:18:12 +0000

    fix: Add js solution to problem 171
    Added a JS solution to find excel sheet
    column number.

---
+++ leetcode/js/pb171_excelsheet_column_number.js
@@ -0,0 +1,7 @@
+var columnNumber = function getColumnNumber(columnTitle) {
+    let result = 0;
+    for (let c of columnTitle) {
+        result = result * 26 + (c.charCodeAt(0) - 'A'.charCodeAt(0) + 1);
+    }
+    return result;
+};
