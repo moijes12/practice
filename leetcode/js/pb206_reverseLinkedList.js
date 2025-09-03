a2d5cb90f72f917110e921fc86f2dea21db46f3c
Alexander Moses <moijes12@gmail.com>
2025-09-03 08:34:15 +0000

    fix: Add solution for 206 reverse linked list
    Added a JS solution to reverse a singly linked
    list.

---
+++ leetcode/js/pb206_reverseLinkedList.js
@@ -0,0 +1,11 @@
+var reverseLinkedList = function(head) {
+    let current = head;
+    let prev = null;
+    while (current) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+};
